95500ca32f64848807419489c273a72f837418a4
Finn Gärtner <finngaertner@protonmail.com>
2024-01-20 00:32:24 +0100

    chores: Update schema

--- src/rewind/schemas.ts
+++ src/rewind/schemas.ts
@@ -1,18 +1,25 @@
 import { z } from "zod"
 
+const artistSchema = z.object({
+  id: z.number(),
+  artistType: z.string(),
+  defaultName: z.string()
+})
+
+const tagSchema = z.object({
+  item1: z.string(),
+  item2: z.number()
+})
+
 export const rewindSchema = z.object({
   songHitsOnDays: z.array(z.object({
     date: z.string(),
     count: z.number()
   })),
-  favoriteGenreTags: z.array(z.object({
-    item1: z.string(),
-    item2: z.number()
-  })),
-  favoriteSubjectiveTags: z.array(z.object({
-    item1: z.string(),
-    item2: z.number()
-  }))
+  favoriteGenreTags: z.array(tagSchema),
+  favoriteSubjectiveTags: z.array(tagSchema),
+  favoriteProducers: z.array(artistSchema),
+  favoriteVoicebanks: z.array(artistSchema)
 })
 
 export type RewindSchema = z.infer<typeof rewindSchema>
