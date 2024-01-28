62a9d7d95a7a3974ce8194b9c5d7cfaa4bcc6f69
Finn Gärtner <finngaertner@protonmail.com>
2024-01-28 21:13:53 +0100

    chores: Change spring config values

--- src/rewind/GenreStats.tsx
+++ src/rewind/GenreStats.tsx
@@ -1,59 +1,91 @@
-import { RewindSchema } from "./schemas"
-import { spring, useCurrentFrame, useVideoConfig } from "remotion"
-import { nivoTheme, renderTick } from "./nivo_utils"
-import { Bar } from "@nivo/bar"
-import { Animated, Fade } from "remotion-animated"
-import { maxByKey } from "./utils"
+import {RewindSchema} from './schemas';
+import {spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {nivoTheme, renderTick} from './nivo_utils';
+import {Bar} from '@nivo/bar';
+import {Animated, Fade} from 'remotion-animated';
+import {maxByKey} from './utils';
 
 interface GenreStatsProps {
-  favoriteGenreTags: RewindSchema["favoriteGenreTags"],
-  favoriteSubjectiveTags: RewindSchema["favoriteSubjectiveTags"]
+	favoriteGenreTags: RewindSchema['favoriteGenreTags'];
+	favoriteSubjectiveTags: RewindSchema['favoriteSubjectiveTags'];
 }
 
-export default function GenreStats({ favoriteGenreTags, favoriteSubjectiveTags }: GenreStatsProps) {
-  const frame = useCurrentFrame()
-  const { fps, width, height } = useVideoConfig()
+export default function GenreStats({
+	favoriteGenreTags,
+	favoriteSubjectiveTags,
+}: GenreStatsProps) {
+	const frame = useCurrentFrame();
+	const {fps, width, height} = useVideoConfig();
 
-  const genreMax = maxByKey(favoriteGenreTags, (g) => g.item2) ?? 1
-  const subjectiveMax = maxByKey(favoriteSubjectiveTags, g => g.item2) ?? 1
+	const genreMax = maxByKey(favoriteGenreTags, (g) => g.item2) ?? 1;
+	const subjectiveMax = maxByKey(favoriteSubjectiveTags, (g) => g.item2) ?? 1;
 
-  const baseData = frame > 150 ? favoriteSubjectiveTags : favoriteGenreTags;
-  const data = baseData.filter((_, ind) => ind < 6).filter(g => g.item1 !== "Other genres")
-    .map((g, ind) => {
-      if (frame < 150) {
-        return { id: g.item1, value: spring({ fps, frame, durationInFrames: 60, delay: 30, from: 0, to: g.item2, config: { stiffness: 50 } }) }
-      }
-      const scale = genreMax / subjectiveMax;
-      const prev = favoriteGenreTags.length > ind ? favoriteGenreTags[ind].item2 : 0;
-      return { id: g.item1, value: spring({ fps, frame, durationInFrames: 90, delay: 150, from: prev, to: g.item2 * scale, config: { stiffness: 50 } }) }
-    })
+	const baseData = frame > 150 ? favoriteSubjectiveTags : favoriteGenreTags;
+	const data = baseData
+		.filter((_, ind) => ind < 6)
+		.filter((g) => g.item1 !== 'Other genres')
+		.map((g, ind) => {
+			if (frame < 150) {
+				return {
+					id: g.item1,
+					value: spring({
+						fps,
+						frame,
+						durationInFrames: 60,
+						delay: 30,
+						from: 0,
+						to: g.item2,
+						config: {stiffness: 50},
+					}),
+				};
+			}
+			const scale = genreMax / subjectiveMax;
+			const prev =
+				favoriteGenreTags.length > ind ? favoriteGenreTags[ind].item2 : 0;
+			return {
+				id: g.item1,
+				value: spring({
+					fps,
+					frame,
+					durationInFrames: 60,
+					delay: 150,
+					from: prev,
+					to: g.item2 * scale,
+					config: {stiffness: 100},
+				}),
+			};
+		});
 
+	data.reverse();
 
-  data.reverse()
-
-  return <Animated in={30} animations={[Fade({ initial: 0, to: 1, duration: 15, start: 30 })]}>
-    <Bar
-      data={data}
-      maxValue={genreMax * 1.2}
-      theme={nivoTheme}
-      height={height * 5 / 6}
-      width={width - 100}
-      layout="horizontal"
-      label=""
-      colors={{ scheme: "nivo" }}
-      colorBy="indexValue"
-      margin={{
-        left: 200,
-        bottom: 50
-      }}
-      enableGridY={false}
-      axisLeft={{
-        renderTick,
-      }}
-      axisBottom={{
-        renderTick: () => <></>
-      }}
-      animate={false}
-    />
-  </Animated>
+	return (
+		<Animated
+			in={30}
+			animations={[Fade({initial: 0, to: 1, duration: 15, start: 30})]}
+		>
+			<Bar
+				data={data}
+				maxValue={genreMax * 1.2}
+				theme={nivoTheme}
+				height={(height * 5) / 6}
+				width={width - 100}
+				layout="horizontal"
+				label=""
+				colors={{scheme: 'nivo'}}
+				colorBy="indexValue"
+				margin={{
+					left: 200,
+					bottom: 50,
+				}}
+				enableGridY={false}
+				axisLeft={{
+					renderTick,
+				}}
+				axisBottom={{
+					renderTick: () => <></>,
+				}}
+				animate={false}
+			/>
+		</Animated>
+	);
 }
--- src/rewind/SongHitText.tsx
+++ src/rewind/SongHitText.tsx
@@ -1,25 +1,49 @@
-import { Animated, Fade } from "remotion-animated";
-import { RewindSchema } from "./schemas";
-import { interpolate, spring, useCurrentFrame, useVideoConfig } from "remotion";
+import {Animated, Fade} from 'remotion-animated';
+import {RewindSchema} from './schemas';
+import {interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
 
 interface SongHitTextProps {
-  songHitsOnDays: RewindSchema["songHitsOnDays"]
+	songHitsOnDays: RewindSchema['songHitsOnDays'];
 }
 
-export default function SongHitText({ songHitsOnDays }: SongHitTextProps) {
-  const { fps } = useVideoConfig()
-  const frame = useCurrentFrame();
+export default function SongHitText({songHitsOnDays}: SongHitTextProps) {
+	const {fps} = useVideoConfig();
+	const frame = useCurrentFrame();
 
-  const count = songHitsOnDays.map(s => s.count).reduce((prev, curr) => prev + curr, 0);
-  const days = songHitsOnDays.length;
+	const count = songHitsOnDays
+		.map((s) => s.count)
+		.reduce((prev, curr) => prev + curr, 0);
+	const days = songHitsOnDays.length;
 
-  const interpolatedCount = interpolate(frame - 30, [0, 60], [0, count], { extrapolateRight: "clamp" })
-  const interpolatedDays = interpolate(frame - 30, [0, 60], [0, days], { extrapolateRight: "clamp" })
+	const interpolatedCount = interpolate(frame - 30, [0, 60], [0, count], {
+		extrapolateRight: 'clamp',
+	});
+	const interpolatedDays = interpolate(frame - 30, [0, 60], [0, days], {
+		extrapolateRight: 'clamp',
+	});
 
-  const scale = spring({ fps, frame, delay: 90, durationInFrames: 60 })
-  const scale2 = interpolate(scale, [0, 1], [500, 0], { extrapolateLeft: "clamp" })
+	const scale = spring({
+		fps,
+		frame,
+		delay: 90,
+		durationInFrames: 60,
+		config: {stiffness: 50, overshootClamping: true, damping: 20},
+	});
+	const scale2 = interpolate(scale, [0, 1], [500, 0]);
 
-  return <Animated in={30} className="flex justify-center " animations={[Fade({ initial: 0, to: 1, start: 30 })]}>
-    <p style={{ transform: `translateY(${scale2}px)` }} className="text-6xl font-bold text-center w-4/5 leading-snug">You've looked at {interpolatedCount.toFixed(0)} song entries on {interpolatedDays.toFixed(0)} days </p>
-  </Animated>
+	return (
+		<Animated
+			in={30}
+			className="flex justify-center "
+			animations={[Fade({initial: 0, to: 1, start: 30})]}
+		>
+			<p
+				style={{transform: `translateY(${scale2}px)`}}
+				className="text-6xl font-bold text-center w-4/5 leading-snug"
+			>
+				You've looked at {interpolatedCount.toFixed(0)} song entries on{' '}
+				{interpolatedDays.toFixed(0)} days{' '}
+			</p>
+		</Animated>
+	);
 }
